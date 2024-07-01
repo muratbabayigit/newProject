e5059e091da075351d164b03ae62e95679558c61
Murat BABAYIGIT <babayigit@gmail.com>
2024-07-01 11:39:32 +0300

    v4

--- src/P03.java
+++ src/P03.java
@@ -2,5 +2,6 @@ public class P03 {
     public static void main(String[] args) {
         System.out.println("yeni modül ekleniyor");
         System.out.println("deneme");
+
     }
 }
---
+++ src/p04.java
@@ -0,0 +1,5 @@
+public class p04 {
+    public static void main(String[] args) {
+        System.out.println("havuclu kek gitti");
+    }
+}
