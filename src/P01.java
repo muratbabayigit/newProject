e24cb6cc9d58fd3d7577b89ea8f34b8bc1798abf
Murat BABAYIGIT <babayigit@gmail.com>
2024-07-01 10:46:41 +0300

    v3.2

--- src/P01.java
+++ src/P01.java
@@ -2,5 +2,6 @@ public class P01 {
     public static void main(String[] args) {
         System.out.println("Merhaba Github");
         System.out.println("Yeni versiyone ekleme");
+        System.out.println("versiyn güncelleme");
     }
 }
--- src/P03.java
+++ src/P03.java
@@ -1,5 +1,6 @@
 public class P03 {
     public static void main(String[] args) {
         System.out.println("yeni modül ekleniyor");
+        System.out.println("deneme");
     }
 }
