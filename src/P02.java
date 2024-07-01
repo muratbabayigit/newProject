31adbc255cd22e147e7e798e8a78fdd27683cc6b
Murat BABAYIGIT <babayigit@gmail.com>
2024-07-01 10:43:16 +0300

    v2.2

--- src/P02.java
+++ src/P02.java
@@ -1,6 +1,9 @@
 public class P02 {
     public static void main(String[] args) {
         System.out.println("İkinci dosya eklendi");
+        System.out.println("Deneme versiyonu");
+
+
 
     }
 }
